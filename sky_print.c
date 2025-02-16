7549738e1753f1b59a4410e0e5b5b1eaddcfd39f
javicast55 <javieer55c@gmail.com>
2025-02-16 18:14:13 +0000

    left

--- sky_print.c
+++ sky_print.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 void up(int *vt);
+void down(int *vt);
+void left(int *vt);
 void board();
 
 int board_ar [6][6] = {{6,0,0,0,0,6},{5,0,0,0,0,5},{5,0,0,0,0,5},
@@ -18,6 +20,36 @@ void    board_up(int v[4])
         board_ar [0][u] = v [u1];
         u++;
         u1++;
+    }
+}
+
+void    board_down(int v[4])
+{
+    int d;
+    int d1;
+
+    d = 1;
+    d1 = 0;
+    while (d < 5 && d1 < 4)
+    {
+        board_ar [5][d] = v [d1];
+        d++;
+        d1++;
+    }
+}
+
+void    board_left(int v[4])
+{
+    int l;
+    int l1;
+
+    l = 1;
+    l1 = 0;
+    while (l < 5 && l1 < 4)
+    {
+        board_ar [l][0] = v [l1];
+        l++;
+        l1++;
     }
 }
 
--- sky_solve.c
+++ sky_solve.c
@@ -1,12 +1,18 @@
 #include "sky_print.c"
 
 void up(int *vt);
+void down(int *vt);
+void left(int *vt);
 
 int upper [4];
+int bottom [4];
+int left_c [4];
 
 int solve(int *vt)
 {
     up(vt);
+    down(vt);
+    left(vt);
     board();
 }
 
@@ -21,4 +27,36 @@ void up(int *vt)
             u++;
     }
     board_up(upper);
+}
+
+void down(int *vt)
+{
+    int d;
+    int d1;
+
+    d = 0;
+    d1 = 4;
+    while (d < 4 && d1 < 8)
+    {
+        bottom [d] = vt[d1];
+            d++;
+            d1++;
+    }
+    board_down(bottom);
+}
+
+void left(int *vt)
+{
+    int l;
+    int l1;
+
+    l = 0;
+    l1 = 8;
+    while (l < 4 && l1 < 12)
+    {
+        left_c [l] = vt[l1];
+            l++;
+            l1++;
+    }
+    board_left(left_c);
 }
